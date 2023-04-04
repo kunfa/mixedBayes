ad745c357ab6ce0d9984d2201f4fd5b28dc7087d
Kun Fan <63618891+kunfa@users.noreply.github.com>
2023-04-03 20:06:20 -0500

    r

--- R/rolong-package.R
+++ R/rolong-package.R
@@ -4,7 +4,7 @@ NULL
 
 #' @docType package
 #' @keywords overview
-#' @name rolon-package
+#' @name rolong-package
 #' @title Bayesian quantile variable selection with mixed effects model in longitudinal study
 #' @aliases rolong-package
 #' @description In longitudinal studies, the same subjects are measured repeatedly over time, leading to correlations among the repeated measurements. Properly accounting for the intra-cluster correlations in the presence of data heterogeneity and long tailed distributions of the disease phenotype is challenging, especially in the context of high dimensional regressions. Here, we aim at developing novel Bayesian regularized quantile mixed effect models to tackle these challenges.In this package, we have implemented the Gibbs samplers of Bayesian quantile variable selection method in the mixed effects model with the spike-and-slab priors. The Markov Chain Monte Carlo (MCMC) algorithms of the proposed and alternative models can be efficiently performed by using the package.
